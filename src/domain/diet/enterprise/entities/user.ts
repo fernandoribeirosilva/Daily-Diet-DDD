f9bbc19ddc4f602c14499f787fbbe1d013a10599
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-06-30 11:16:57 -0300

    fix: foi removido propriedade photo.

--- src/domain/diet/enterprise/entities/user.ts
+++ src/domain/diet/enterprise/entities/user.ts
@@ -1,12 +1,10 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Photo } from './value-objects/Photo'
 
 export interface UserProps {
   name: string
   email: string
   password: string
-  photo: Photo
 }
 
 export class User extends Entity<UserProps> {
@@ -16,10 +14,6 @@ export class User extends Entity<UserProps> {
 
   get email() {
     return this.props.email
-  }
-
-  get photo() {
-    return this.props.photo
   }
 
   get password() {
