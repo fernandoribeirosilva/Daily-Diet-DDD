af620ebce10e0b166356ac58b490ea96fd04b81b
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-06-30 11:09:54 -0300

    feat: criado método para um usuário pelo email

--- src/domain/diet/application/repositories/IUser-repository.ts
+++ src/domain/diet/application/repositories/IUser-repository.ts
@@ -1,5 +1,6 @@
 import { User } from '../../enterprise/entities/user'
 
 export interface IUserRepository {
+  findByEmail(email: string): Promise<User | null>
   create(user: User): Promise<void>
 }
